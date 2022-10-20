092c0d681a7a085425d77a1872dac973b0977e4c
onepunchcat <115331116+onepunchcat@users.noreply.github.com>
2022-10-20 18:04:53 +0800

    chore: remove generater backend api

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -3,26 +3,6 @@ import { $fetch } from 'ohmyfetch'
 import { GeneraterParameters } from '../types'
 import { getViteEnv } from '../utils'
 
-function getGeneraterAPIBaseUrl(path: string): string {
-  const baseUrl = getViteEnv('VITE_GEN_BASE_URL')
-  return new URL(path, baseUrl).href
-}
-
-const generaterFetch = $fetch.create({ baseURL: getGeneraterAPIBaseUrl('/') })
-
-type GenerateImageResult = { output: string[] }
-
-export async function generateImage(param: GeneraterParameters): Promise<string> {
-  const res = await generaterFetch<GenerateImageResult>('/generate', {
-    method: 'POST',
-    mode: 'cors',
-    referrerPolicy: 'no-referrer',
-    headers: { accept: '*/*', 'content-type': 'application/json' },
-    body: { ...param },
-  })
-  return res.output[0]
-}
-
 function getMinterAPIBaseUrl(path: string): string {
   const baseUrl = getViteEnv('VITE_MINT_BASE_URL')
   return new URL(path, baseUrl).href
